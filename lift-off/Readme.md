2695fee36b386f4f116dbd538666650b100663ab
Mahnoor Fatima Faizi <mahnoorf2002@gmail.com>
2024-11-07 15:03:59 +0500

    Problem 03

---
+++ lift-off/Readme.md
@@ -0,0 +1,3 @@
+Problem Statement
+
+Write a program that prints out the calls for a spaceship that is about to launch. Countdown from 10 to 1 and then output Liftoff!
---
+++ lift-off/main.py
@@ -0,0 +1,18 @@
+def countdown1():
+    for i in range(10, 0, -1):
+        print(i, end=" ")
+    print("Lift Off!")
+
+countdown1()
+
+# List with for loop
+
+number_list = [10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0]
+def countdown2():
+    for i in number_list:
+        print(i, end= " ")
+
+    print("Lift Off!")
+
+countdown2()
+
